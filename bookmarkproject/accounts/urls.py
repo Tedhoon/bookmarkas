bd24667c7df2181ceb448cdff594a0f153a78e4a
Tedhoon <gt0305@likelion.org>
2019-07-22 18:52:27 +0900

    bookmarkproject

---
+++ bookmarkproject/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_view
+from .views import register
+
+urlpatterns = [
+    path('login/', auth_view.LoginView.as_view(template_name = 'login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name ='registartion/logout.html'), name="logout"),
+    path('register/', register, name="register")
+]
